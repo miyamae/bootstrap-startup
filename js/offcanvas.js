52c21746d1bcf66b6458f8fdcbaa8e1442652bf2
Tatsuya Miyamae <miyamae@pixy.ne.jp>
2013-09-06 16:53:27 +0900

    サイドバーがコンテンツより高いとモバイル表示時にフッタがサイドバーの上にかぶってしまうのを修正。

--- js/offcanvas.js
+++ js/offcanvas.js
@@ -1,8 +1,5 @@
 $(function () {
     $('[data-toggle=offcanvas]').click(function() {
-//		alert('text');
 		$('.row-offcanvas').toggleClass('active');
-		$('#sidebar').show();
-		console.log($('#sidebar'));
 	});
 });
